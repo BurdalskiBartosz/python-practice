557fd197ae6d42d7a3583649ac76bf631f8fe3ef
Bartosz <bartosz.burdalski@gmail.com>
2025-01-18 14:59:00 +0100

    Use with context manager and add side scripts

--- app.py
+++ app.py
@@ -6,31 +6,50 @@ while True:
         case 'add':
             todo = input("Enter a todo: ") + "\n"
 
-            file = open('files/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('files/todos.txt') as file:
+                todos = file.readlines()
+
             todos.append(todo)
 
-            file = open('files/todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'show':
-            file = open('files/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
 
             for index, item in enumerate(todos):
+                item = item.strip('\n')
                 row = f"{index + 1}-{item}"
                 print(row)
         case 'edit':
-            number = int(input('Number of the todo to edit: r'))
+            number = int(input('Number of the todo to edit: '))
             number = number - 1
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
             new_todo = input('Enter new todo: ')
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'complete':
             number = int(input('Number of the todo to complete: '))
-            todos.pop(number-1)
 
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+            index = number -1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list."
+            print(message)
         case 'exit':
             break
 
