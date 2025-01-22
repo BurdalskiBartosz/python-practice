c7d69d6a01ca3736f664cb03b24f1a3124d3719a
Bartosz <bartosz.burdalski@gmail.com>
2025-01-22 21:51:53 +0100

    Update app and script6

--- sidescripts/script6.py
+++ sidescripts/script6.py
@@ -1,15 +1,19 @@
 feet_inches = input("Enter feet and inches: ")
 
-def convert(feet_inches):
-    parts = feet_inches.split(" ")
+def parse(feet_inches_input):
+    parts = feet_inches_input.split(" ")
     feet = float(parts[0])
     inches = float(parts[1])
 
+    return feet, inches
+
+
+def convert(feet, inches):
     meters = feet * 0.3048 + inches * 0.0254
     return meters
 
-
-result = convert(feet_inches)
+f, i  = parse(feet_inches)
+result = convert(f,i)
 
 if result < 1:
     print("Kid is too small.")
