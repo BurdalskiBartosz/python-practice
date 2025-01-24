178d9628231af9f54ade1d6cacc7ad7f3c940208
Bartosz <bartosz.burdalski@gmail.com>
2025-01-24 11:13:42 +0100

    Play with python modules

---
+++ e2.py
@@ -0,0 +1,13 @@
+import csv
+
+with open("weather.csv", 'r') as file:
+    data = list(csv.reader(file))
+
+city = input("Enter a city: ")
+
+print(data)
+print(data[1:])
+
+for row in data[1:]:
+    if row[0] == city:
+        print(row[1])
