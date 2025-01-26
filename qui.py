38733808286546476c7b9e4bb91897c356af0912
Bartosz <bartosz.burdalski@gmail.com>
2025-01-26 15:45:49 +0100

    Update

--- qui.py
+++ qui.py
@@ -1,6 +1,11 @@
 import functions
 import FreeSimpleGUI as sg
 import time
+import os
+
+if not os.path.exists(functions.FILEPATH):
+    with open(functions.FILEPATH, 'w') as file:
+        pass
 
 sg.theme("Black")
 
